1a87c1b7f854fcc737267978f68c39d9099e5c50
leocb2 <jcrv54@protonmail.com>
2023-07-02 00:20:29 +0100

    incomplete rate statistics

--- main.py
+++ main.py
@@ -29,6 +29,5 @@ if __name__ == "__main__":
 
     while True:
         sniff(iface=interface,prn=monitor.monitor_packets_size, store=0)
-        print("lol")
 
     # Parallel(n_jobs=2)([delayed(sniff_packets)(), delayed(monitor_packets)()])
--- monitor_traffic.py
+++ monitor_traffic.py
@@ -6,9 +6,14 @@ from io import StringIO
 
 class MonitorTraffic:
     packets = []
+    packets_timestamps = []
     unusual_packet_size = []
 
     def __init__(self,sniff_time,threshold,reset_time = 86400):
+
+        if sniff_time <= 0:
+            raise ValueError("sniff_time must be higher than 0.")
+        
         self.logger = logging.getLogger(__name__)
         self.logger.info("MonitorTraffic instance created.")
         self.sniff_time = sniff_time
@@ -21,19 +26,30 @@ class MonitorTraffic:
             self.packets = []
 
         self.packets.append(packet)
-        self.logger.info(f"Packet registered. Total packets: {len(self.packets)}")
+        self.packets_timestamps.append(packet.time)
+        # self.logger.info(f"Packet registered. Total packets: {len(self.packets)}")
 
-    def _get_packets_size_mean(self):
+    def _calculate_packet_rate(self):
+        current_time = time.time()
+
+        # filter packet timestamps
+        recent_packets = [p for p in self.packets if p >= current_time - self.sniff_time]
+        
+        # calculate packet rate for this interval
+        packet_rate = len(recent_packets) / self.sniff_time
+        self.packet_rates.append(packet_rate)
+
+    def _get_packets_rate_mean(self):
         if len(self.packets) >= 1:
-            return statistics.mean([len(p) for p in self.packets])
+            return statistics.mean(len(self.packets)/self.sniff_time)
         else:
-            return 0
+            return None
     
-    def _get_packets_size_std(self):
+    def _get_packets_rate_std(self):
         if len(self.packets) >= 2:
-            return statistics.stdev([len(p) for p in self.packets])
+            return statistics.stdev(len(self.packets)/self.sniff_time)
         else:
-            return 0
+            return None
 
     def monitor_packets_size(self,packet):
         self._register_packet(packet)
@@ -41,21 +57,18 @@ class MonitorTraffic:
 
         # filter packets
         packets_interval = [p for p in self.packets if p.time >= current_time - self.sniff_time]
-
-        # get packet sizes
-        packet_sizes = [len(p) for p in packets_interval]
-
+        
         # packets statistics
-        if len(packet_sizes) < 1:
+        if len(packets_interval) < 1:
             self.logger.info("No packets captured")
             return
         
-        mean_packet_size = self._get_packets_size_mean()
-        std_packet_size = self._get_packets_size_std()
+        mean_packet_rate = self._get_packets_rate_mean()
+        std_packet_rate = self._get_packets_rate_std()
 
-        total_size = sum(packet_sizes)
+        total_size = len(packets_interval)
 
-        if total_size > mean_packet_size + self.threshold*std_packet_size:
+        if total_size > mean_packet_rate + self.threshold*std_packet_rate:
             self.unusual_packet_size.append((total_size,packets_interval))
 
             self.logger.info(f"Unusual traffic volume detected: {total_size} bytes in the last {self.sniff_time} seconds. Packets:\n")
